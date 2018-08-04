e4f39b1cfbd9d9095d4c9eeb013f4883652d35c7
Sema <sema@pyva.net>
2018-08-04 19:13:46 -0400

    Version upgrade

--- javascript/JZZ.js
+++ javascript/JZZ.js
@@ -12,7 +12,7 @@
 })(this, function() {
 
   var _scope = typeof window === 'undefined' ? global : window;
-  var _version = '0.5.2';
+  var _version = '0.5.3';
   var i, j, k, m, n;
 
   var _time = Date.now || function () { return new Date().getTime(); };
