281b6db13546fff107522e1ca8f4a71d59e09e11
Sema <sema@pyva.net>
2018-11-01 01:45:34 -0400

    Node v11.x.x compatible

--- javascript/JZZ.js
+++ javascript/JZZ.js
@@ -12,7 +12,7 @@
 })(this, function() {
 
   var _scope = typeof window === 'undefined' ? global : window;
-  var _version = '0.5.9';
+  var _version = '0.6.0';
   var i, j, k, m, n;
 
   var _time = Date.now || function () { return new Date().getTime(); };
