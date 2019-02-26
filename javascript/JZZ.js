78dc8456b31e8f0efddcf577169e39d927cc1edf
Sema <sema@pyva.net>
2019-02-25 21:53:53 -0500

    Allow invalid key signature in MIDI files

--- javascript/JZZ.js
+++ javascript/JZZ.js
@@ -13,7 +13,7 @@
 })(this, function() {
 
   var _scope = typeof window === 'undefined' ? global : window;
-  var _version = '0.6.8';
+  var _version = '0.6.9';
   var i, j, k, m, n;
 
   var _time = Date.now || function () { return new Date().getTime(); };
@@ -1546,7 +1546,7 @@
         81: _helperSMF.smfTempo,
         84: _helperSMF.smfSMPTE,
         88: _helperSMF.smfTimeSignature,
-        89: _helperSMF.smfKeySignature,
+        89: function(dd) { if (dd.length == 2) return _smf(89, dd); throw RangeError('Incorrect key signature: ' + _smftxt(dd)); },
         127: _helperSMF.smfMetaEvent
       }[ff];
       return f ? f(typeof dd == 'undefined' ? '' : _8bs(dd)) : _smf(ff, dd);
