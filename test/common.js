5f2981ad526970f2fe642b31a57bd9d4a53e4720
Sema <sema@pyva.net>
2019-05-05 01:56:44 -0400

    Split tests

--- test/common.js
+++ test/common.js
@@ -1,12 +1,5 @@
 ﻿var assert = require('assert');
 var JZZ = require('..');
-var MT;
-try {
-  MT = require('midi-test');
-}
-catch(err) {
-  console.log('midi-test module is disabled in this configuration');
-}
 
 function Sample(done, list) {
   this.done = done;
@@ -23,7 +16,7 @@ describe('Info', function() {
   console.log('Node:', process.versions.node);
   console.log('process.platform:', process.platform);
   console.log('process.arch:', process.arch);
-  console.log('JZZ:', JZZ().info().ver);
+  console.log('JZZ:', JZZ('none').info().ver);
 });
 
 describe('MIDI messages', function() {
@@ -346,32 +339,11 @@ describe('JZZ.Widget', function() {
 
 describe('Engine', function() {
   it('JZZ()', function(done) {
-    JZZ().wait(0).wait(1).and(function() { console.log(this.info()); done(); });
+    JZZ('none').wait(0).wait(1).and(function() { console.log(this.info()); done(); });
   });
   it('Non-existing port', function(done) {
-    JZZ().openMidiOut('Non-existing port').or(function() { done(); });
+    JZZ('none').openMidiOut('Non-existing port').or(function() { done(); });
   });
-  if (process.platform == 'win32' || process.platform == 'darwin') {
-    it('Default MIDI-Out', function(done) {
-      JZZ().openMidiOut().and(function() { this.close(); done(); });
-    });
-  }
-  if (process.platform == 'win32') {
-    it('Microsoft GS Wavetable Synth', function(done) {
-      JZZ().openMidiOut('Microsoft GS Wavetable Synth').and(function() { this.close(); done(); });
-    });
-    it('/Microsoft/', function(done) {
-      JZZ().openMidiOut(/Microsoft/).and(function() { this.close(); done(); });
-    });
-  }
-  if (process.platform == 'darwin') {
-    it('Apple DLS Synth', function(done) {
-      JZZ().openMidiOut('Apple DLS Synth').and(function() { this.close(); done(); });
-    });
-    it('/Apple/', function(done) {
-      JZZ().openMidiOut(/Apple/).and(function() { this.close(); done(); });
-    });
-  }
   it('Dummy MIDI-Out', function(done) {
     JZZ.lib.registerMidiOut('Dummy MIDI-Out', {
       _info: function(name) { return { name: name }; },
@@ -382,26 +354,8 @@ describe('Engine', function() {
         port._resume();
       }
     });
-    var out = JZZ().openMidiOut('Dummy MIDI-Out').and(function() { this.noteOn(0, 60); });
+    var out = JZZ('none').openMidiOut('Dummy MIDI-Out').and(function() { this.noteOn(0, 60); });
   });
-  if (MT && (process.platform == 'darwin' || process.platform == 'linux')) {
-    it('Virtual MIDI-In', function(done) {
-      var src = MT.MidiSrc('Virtual MIDI-In');
-      src.connect();
-      JZZ().openMidiIn('Virtual MIDI-In').and(function() { this.close(); src.disconnect(); done(); });
-    });
-    it('Virtual MIDI-Out', function(done) {
-      var port;
-      var dst = MT.MidiDst('Virtual MIDI-Out');
-      dst.connect();
-      dst.receive = function(msg) {
-        port.close();
-        dst.disconnect();
-        done();
-      };
-      port = JZZ().openMidiOut('Virtual MIDI-Out').and(function() { this.noteOn(0, 60, 127); });
-    });
-  }
 });
 
 describe('Web MIDI API', function() {
@@ -410,16 +364,5 @@ describe('Web MIDI API', function() {
     function onFail(err) { console.log('requestMIDIAccess failed!', err); }
     JZZ.requestMIDIAccess().then(onSuccess, onFail);
   });
-  if (MT && (process.platform == 'darwin' || process.platform == 'linux')) {
-    it.skip('onstatechange', function(done) {
-      var src = MT.MidiSrc('Virtual MIDI-In');
-      function onSuccess(midiaccess) {
-        midiaccess.onstatechange = function() { src.disconnect(); done(); };
-        setImmediate(function() { src.connect(); });
-      }
-      function onFail(err) { console.log('requestMIDIAccess failed!', err); }
-      JZZ.requestMIDIAccess().then(onSuccess, onFail);
-    });
-  }
 });
 
---
+++ test/engine1.js
@@ -0,0 +1,76 @@
+﻿var assert = require('assert');
+var JZZ = require('..');
+var MT;
+try {
+  MT = require('midi-test');
+}
+catch(err) {
+  console.log('midi-test module is disabled in this configuration');
+}
+
+function Sample(done, list) {
+  this.done = done;
+  this.list = list.slice();
+  this.count = 0;
+  this.compare = function(msg) {
+    if (this.count < this.list.length) assert.equal(msg.slice().toString(), this.list[this.count].toString());
+    this.count++;
+    if (this.count == this.list.length) this.done();
+  };
+}
+
+describe('Engine', function() {
+  if (process.platform == 'win32' || process.platform == 'darwin') {
+    it('Default MIDI-Out', function(done) {
+      JZZ().openMidiOut().and(function() { this.close(); done(); });
+    });
+  }
+  if (process.platform == 'win32') {
+    it('Microsoft GS Wavetable Synth', function(done) {
+      JZZ().openMidiOut('Microsoft GS Wavetable Synth').and(function() { this.close(); done(); });
+    });
+    it('/Microsoft/', function(done) {
+      JZZ().openMidiOut(/Microsoft/).and(function() { this.close(); done(); });
+    });
+  }
+  if (process.platform == 'darwin') {
+    it('Apple DLS Synth', function(done) {
+      JZZ().openMidiOut('Apple DLS Synth').and(function() { this.close(); done(); });
+    });
+    it('/Apple/', function(done) {
+      JZZ().openMidiOut(/Apple/).and(function() { this.close(); done(); });
+    });
+  }
+  if (MT && (process.platform == 'darwin' || process.platform == 'linux')) {
+    it('Virtual MIDI-In', function(done) {
+      var src = MT.MidiSrc('Virtual MIDI-In');
+      src.connect();
+      JZZ().openMidiIn('Virtual MIDI-In').and(function() { this.close(); src.disconnect(); done(); });
+    });
+    it('Virtual MIDI-Out', function(done) {
+      var port;
+      var dst = MT.MidiDst('Virtual MIDI-Out');
+      dst.connect();
+      dst.receive = function(msg) {
+        port.close();
+        dst.disconnect();
+        done();
+      };
+      port = JZZ().openMidiOut('Virtual MIDI-Out').and(function() { this.noteOn(0, 60, 127); });
+    });
+  }
+});
+
+describe('Web MIDI API', function() {
+  if (MT && (process.platform == 'darwin' || process.platform == 'linux')) {
+    it.skip('onstatechange', function(done) {
+      var src = MT.MidiSrc('Virtual MIDI-In');
+      function onSuccess(midiaccess) {
+        midiaccess.onstatechange = function() { src.disconnect(); done(); };
+        setImmediate(function() { src.connect(); });
+      }
+      function onFail(err) { console.log('requestMIDIAccess failed!', err); }
+      JZZ.requestMIDIAccess().then(onSuccess, onFail);
+    });
+  }
+});
