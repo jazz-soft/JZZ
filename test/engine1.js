c349e58cc9fa5638c953e5b6ffe2570ed83ad0ec
Sema <sema@pyva.net>
2019-08-25 01:08:37 -0400

    Re-enabled fixed tests

--- test/engine1.js
+++ test/engine1.js
@@ -38,11 +38,11 @@ describe('Engine: node', function() {
   test.web_midi_output_no_sysex();
   test.web_midi_output_sysex();
   if (MT) {
-    //test.web_midi_input_connect();
-    //test.web_midi_input_disconnect();
+    test.web_midi_input_connect();
+    test.web_midi_input_disconnect();
     //test.web_midi_input_reconnect();
-    //test.web_midi_output_connect();
-    //test.web_midi_output_disconnect();
+    test.web_midi_output_connect();
+    test.web_midi_output_disconnect();
     //test.web_midi_output_reconnect();
   }
   test.close_engine();
