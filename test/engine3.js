e0bcf79dc41a0d775a4e2e115e2b26c17f73e8c3
Sema <sema@pyva.net>
2019-09-05 01:35:59 -0400

    More coverage

--- test/engine3.js
+++ test/engine3.js
@@ -10,6 +10,20 @@ var WMT = require('web-midi-test');
 WMT.sysex = false;
 global.navigator = WMT;
 
+global.document = {
+  handle: {},
+  addEventListener: function(name, handle) { this.handle[name] = handle; },
+  removeEventListener: function(name /*, handle*/) { delete this.handle[name]; },
+};
+window.dispatchEvent = function(evt) { if (document.handle[evt.name]) document.handle[evt.name](); };
+window.webkitAudioContext = function() {
+  return {
+    resume: function() { this.state = 'running'; },
+    createOscillator: function() { return { connect: function() {}, noteOn: function() {}, noteOff: function() {} }; },
+    createGainNode: function() { return { connect: function() {}, gain: { setTargetAtTime: function() {} } }; }
+  };
+};
+
 var test = require('./tests.js')(JZZ, { engine: 'webmidi', sysex: true, degrade: true }, WMT);
 
 describe('Engine: webmidi', function() {
