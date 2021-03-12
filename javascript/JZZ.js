047fdc26372c3bb96b75f6571c161a304e6158a7
Sema <jazz-dev@jazz-soft.net>
2021-03-11 22:12:36 -0500

    Bump the version

--- javascript/JZZ.js
+++ javascript/JZZ.js
@@ -14,7 +14,7 @@
 })(this, function() {
 
   var _scope = typeof window === 'undefined' ? global : window;
-  var _version = '1.2.4';
+  var _version = '1.2.5';
   var i, j, k, m, n;
 
   var _time = Date.now || function () { return new Date().getTime(); };
