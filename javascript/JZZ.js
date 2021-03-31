ee9f831d3b6a9f78b9dd7d28cb4d136109ed31df
Sema <jazz-dev@jazz-soft.net>
2021-03-30 23:11:27 -0400

    Link fix

--- javascript/JZZ.js
+++ javascript/JZZ.js
@@ -14,7 +14,7 @@
 })(this, function() {
 
   var _scope = typeof window === 'undefined' ? global : window;
-  var _version = '1.2.6';
+  var _version = '1.2.7';
   var i, j, k, m, n;
 
   var _time = Date.now || function () { return new Date().getTime(); };
