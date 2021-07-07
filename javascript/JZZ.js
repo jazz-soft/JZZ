9ef8ea24d8ec1e9e83388caa6c04df33cbcfefa0
Sema <jazz-dev@jazz-soft.net>
2021-07-06 23:36:43 -0400

    New release

--- javascript/JZZ.js
+++ javascript/JZZ.js
@@ -14,7 +14,7 @@
 })(this, function() {
 
   var _scope = typeof window === 'undefined' ? global : window;
-  var _version = '1.3.5';
+  var _version = '1.3.6';
   var i, j, k, m, n;
 
   var _time = Date.now || function () { return new Date().getTime(); };
