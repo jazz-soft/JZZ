409cacd8651aaeb194008a818c2de0e7ec1c1c1f
Sema <jazz-dev@jazz-soft.net>
2023-12-20 00:01:38 -0500

    New release

--- javascript/JZZ.js
+++ javascript/JZZ.js
@@ -14,7 +14,7 @@
 })(this, function() {
 
   var _scope = typeof window === 'undefined' ? global : window;
-  var _version = '1.7.6';
+  var _version = '1.7.7';
   var i, j, k, m, n;
 
   /* istanbul ignore next */
