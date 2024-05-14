a61ad0e221e321a04fd449cd115db87b8d8638c3
Sema <jazz-dev@jazz-soft.net>
2024-05-13 20:06:05 -0400

    Updated dependencies

--- javascript/JZZ.js
+++ javascript/JZZ.js
@@ -14,7 +14,7 @@
 })(this, function() {
 
   var _scope = typeof window === 'undefined' ? global : window;
-  var _version = '1.8.2';
+  var _version = '1.8.3';
   var i, j, k, m, n;
 
   /* istanbul ignore next */
