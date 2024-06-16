e857e3da3f36d2904309b600d6024be3418c2b46
Sema <jazz-dev@jazz-soft.net>
2024-06-16 12:05:17 -0400

    eslint.config.mjs

---
+++ eslint.config.mjs
@@ -0,0 +1,38 @@
+import globals from "globals";
+import js from "@eslint/js";
+
+export default [
+    js.configs.recommended,
+    {
+        ignores: ["**/.instrumented/*"]
+    },
+    {
+        files: ["javascript/*.js", "test/*.js"],
+        languageOptions: {
+            ecmaVersion: 2015,
+            globals: {
+                ...globals.browser,
+                ...globals.node,
+                define: "readonly",
+                JZZ: "readonly"
+            }
+        },
+        rules: {
+            "no-empty" : ["error", { "allowEmptyCatch": true }],
+            "no-prototype-builtins": "off",
+            "no-unused-vars": ["error", { caughtErrors: "none"}]
+        }
+    },
+    {
+        files: ["test/*.js"],
+        languageOptions: {
+            ecmaVersion: 2017,
+            globals: {
+                describe: "readonly",
+                it: "readonly",
+                before: "readonly",
+                after: "readonly"
+            }
+        }
+    }
+];
