06882f3c533a38315f2ca830cc556035b7195929
Sema <jazz-dev@jazz-soft.net>
2025-04-07 19:48:43 -0400

    Minor cleanup

--- javascript/JZZ.js
+++ javascript/JZZ.js
@@ -14,7 +14,7 @@
 })(this, function() {
 
   var _scope = typeof window === 'undefined' ? global : window;
-  var _version = '1.9.2';
+  var _version = '1.9.3';
   var i, j, k, m, n;
 
   /* istanbul ignore next */
